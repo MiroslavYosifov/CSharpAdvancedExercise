a865590d4766f884a6096d7ecfde5d666227acae
MiroslavYosifov <m.yosifov193@gmail.com>
2017-06-02 17:03:39 +0300

    Add new exercise

---
+++ 09 Calculate Sequence with Queue/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Calculate_Sequence_with_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = long.Parse(Console.ReadLine());
+            var queue = new Queue<long>();
+            var queue2 = new Queue<long>();
+
+            queue.Enqueue(n);
+            queue2.Enqueue(n);
+                   
+            for (int i = 0; i < 17; i++)
+            {
+                CalculateNumbers(queue, queue2);
+            }
+
+            var rolls = queue.Count - 2;
+
+            for (int i = 0; i < rolls; i++)
+            {
+                Console.Write(queue.Dequeue());
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+
+        private static void CalculateNumbers(Queue<long> queue, Queue<long> queue2)
+        {
+            long s1 = queue2.Dequeue();
+            long s2 = s1 + 1;
+            long s3 = (s1 * 2) + 1;
+            long s4 = s1 + 2;
+
+            queue.Enqueue(s2);
+            queue.Enqueue(s3);
+            queue.Enqueue(s4);
+
+            queue2.Enqueue(s2);
+            queue2.Enqueue(s3);
+            queue2.Enqueue(s4);
+        }
+
+    }
+}
---
+++ 10 - Basic Queue Operations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04___Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < input[0]; i++)
+            {
+                stack.Push(numbers[i]);
+            }
+
+            for (int i = 0; i < input[1]; i++)
+            {
+                stack.Pop();
+            }
+
+            if (!stack.Contains(input[2]) && stack.Count > 0)
+            {
+                Console.WriteLine("true");
+            }
+            else if (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Min());
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
